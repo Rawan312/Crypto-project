b5ef653818c39d42de0a579570f9b7e4a214330a
Rawan312 <155899963+Rawan312@users.noreply.github.com>
2024-01-07 17:41:52 +0200

    Create Crypto.py

---
+++ Crypto.py
@@ -0,0 +1,4 @@
+from Crypto.Cipher import DES3  #provide DES encryption algorithm
+from Crypto.PublicKey import RSA #PROVIDE RSA key generation
+from Crypto.Cipher import PKCS1_OAEP # RSA encryption
+from Crypto.Random import get_random_bytes #generate random bytes
